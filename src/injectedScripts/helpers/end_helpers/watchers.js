fe1c57fb61a16d1d8bb064f53fafa1caee3b603c
Zed Unknown <116058155+ZedUnknown@users.noreply.github.com>
2026-07-17 10:06:41 +0530

    Remove debug log for userInputContainer change detection

--- src/injectedScripts/helpers/end_helpers/watchers.js
+++ src/injectedScripts/helpers/end_helpers/watchers.js
@@ -1,9 +1,6 @@
-const DEBUG = false;
-
 // attach observer to userInputContainer to detect changes
 window.getUserInputContainer().then((userInputContainer) => {
 	window.observeElementChange(userInputContainer, () => {
-		if (DEBUG) console.log(`${PREFIX} userInputContainer changed...`);
 		window.getUserInputContainer().then((container) => {
 			window.userInputContainer = container;
 		})
